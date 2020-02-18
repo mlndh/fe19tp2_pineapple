161807a5d71e05ddade9395cce5d2f53603f49c6
malin DH <m.hjerpe@live.se>
2020-02-18 19:44:20 +0100

    mapreduce

---
+++ src/components/GraphForm/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target);
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            name="brand"
+            type="text"
+            value={this.state.brand}
+            onChange={this.handleChange}
+          />
+          <input
+            name="year"
+            type="text"
+            value={this.state.year}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default NameForm;
