965324e1714f6f77a6c1d30a1bb3e3fb23d51dbf
Robingorski <robin.gorski@student.kyh.se>
2020-02-26 11:12:20 +0100

    moved car companies down to the KPI-report page, and also styled them

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+import styled, { createGlobalStyle } from "styled-components";
+
+const Kpidiv = styled.div `
+    display: flex;
+    justify-content: space-evenly;
+`
+
+const Styledlinks = styled.div`
+  a {
+    font-size: 1.2rem;
+    font-weight: 600;
+    text-decoration: none;
+    list-style: none;
+    color: black;
+  }
+`;
+
 const App = () => (
   <div>
-    <h1>Here is the landing page</h1>
+    <Kpidiv>
+     <Styledlinks>
+      <Link to={ROUTES.Chartmoller}>Möller bil</Link>
+    </Styledlinks>
+    <Styledlinks>
+      <Link to={ROUTES.Chartbrabil}>Brabil</Link>
+    </Styledlinks>
+    <Styledlinks>
+      <Link to={ROUTES.Chartus}>US center</Link>
+    </Styledlinks>
+    </Kpidiv>
+    <h1>Here is the landing page (here should the KPI reports show instead)</h1>
   </div>
 );
 export default App;
