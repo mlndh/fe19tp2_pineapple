5057d6ed13406438107350fa4ce3301adaf9b544
AdrianaAazami <adriana.aazami@outlook.com>
2020-03-02 12:08:47 +0100

    Chart updates

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,46 +13,69 @@ import { withAuthentication } from "../Session";
 import APIDATA from "../APIDATA";
 import GraphForm from "../GraphForm";
 import { volvo, averagePrice } from "./data";
+import AddButton from '../AddButton';
+import BarChart from '../BarChart';
 
-import Chartdacia from '../Dacia/chartdacia'
-import Chartbmw from '../Bmw/chartbmw'
-import Chartaudi from '../Audi/chartaudi';
-import Chart from "../TestGraph/Chart.js";
-import Chartmoller from "../Mollerbil/chartmoller";
-import Chartbrabil from "../Brabil/chartbrabil";
-import Chartus from "../Usbil/chartus";
-
+// import Chart from "../Charts/Chart.js";
+import MultiChart from "../Charts/MultiChart";
+/* import MultiChart from "../Charts/MultiChart"; */
 
 //import { averagePrice } from "./data";
 
-const App = () => (
-  <React.Fragment>
-    <Router>
-      <div>
-        <Navigation />
-        <hr />
-        <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-        <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} />
-        <Route path={ROUTES.APIDATA} component={APIDATA} />
-        <Route path={ROUTES.Chartmoller} component={Chartmoller} />
-        <Route path={ROUTES.Chartbrabil} component={Chartbrabil} />
-        <Route path={ROUTES.Chartus} component={Chartus} />
-      </div>
-    </Router>
-    < Chart />
-    < Chartmoller />
-    < Chartbrabil />
-    < Chartus />
-    < Chartaudi />
-    < Chartbmw />
-    < Chartdacia />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
 
-  </React.Fragment>
-);
+    this.handleAddChart = this.handleAddChart.bind(this);
+    this.handleChartBrandChange = this.handleChartBrandChange.bind(this);
+    this.state = { charts: ['Audi'] }
+  }
+
+  handleAddChart() {
+    const chartArray = this.state.charts;
+    chartArray.push('Audi');
+    this.setState({ charts: chartArray })
+  }
+
+  handleChartBrandChange(index, brand) {
+    //console.log("index: " + index + ", brand: " + brand);
+    const chartArray = this.state.charts;
+    chartArray[index] = brand;
+    this.setState({ charts: chartArray })
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <div>
+            <Navigation />
+            <hr />
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+            <Route path={ROUTES.HOME} component={HomePage} />
+            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+            <Route path={ROUTES.ADMIN} component={AdminPage} />
+            <Route path={ROUTES.APIDATA} component={APIDATA} />
+          </div>
+        </Router>
+        <AddButton handleAddChart={this.handleAddChart} />
+        <div className='chartContainer'>
+          {this.state.charts.map((chart, index) => {
+            return (
+              <MultiChart handleChartBrandChange={this.handleChartBrandChange} index={index} />
+            )
+          })}
+          {/* <MultiChart /> */}
+          {/* < Chart /> */}
+          <BarChart brands={this.state.charts} />
+        </div>
+
+      </React.Fragment>
+    );
+  }
+}
+
 export default withAuthentication(App);
---
+++ src/components/Charts/MultiChart/multiyears.js
@@ -0,0 +1,113 @@
+import { Audiyears } from '../audi';
+import { bmwyears } from '../bmw';
+import { citroenyears } from '../citroen';
+import { daciayears } from '../dacia';
+import { fiatyears } from '../fiat';
+import { fordyears } from '../ford';
+import { hyundaiyears } from '../hyundai';
+import { hondayears } from '../honda';
+import { jaguaryears } from '../jaguar';
+import { kiayears } from '../kia';
+import { ldyears } from '../landrover';
+import { mazdayears } from '../mazda';
+import { mbyears } from '../mercedes';
+import { nissanyears } from '../nissan';
+import { opelyears } from '../opel';
+import { porscheyears } from '../porsche';
+import { renaultyears } from '../renault';
+import { seatyears } from '../seat';
+import { skodayears } from '../skoda';
+import { vsyears } from '../volkswagen';
+import { volvoyears } from '../volvo';
+
+
+
+
+
+
+
+
+
+export const multi = {
+    Audi: Audiyears.results,
+    Bmw: bmwyears.results,
+    Citroën: citroenyears.results,
+    Dacia: daciayears.results,
+    Fiat: fiatyears.results,
+    Ford: fordyears.results,
+    Hyundai: hyundaiyears.results,
+    Honda: hondayears.results,
+    Jaguar: jaguaryears.results,
+    KIA: kiayears.results,
+    Landrover: ldyears.results,
+    Mazda: mazdayears.results,
+    MercedesBenz: mbyears.results,
+    Nissan: nissanyears.results,
+    Opel: opelyears.results,
+    Porsche: porscheyears.results,
+    Renault: renaultyears.results,
+    Seat: seatyears.results,
+    Skoda: skodayears.results,
+    Volkswagen: vsyears.results,
+    Volvo: volvoyears.results,
+
+
+
+
+
+
+
+
+
+}
+
+
+// ytterst: märket. exempel:
+// hyundai: [{...}],
+/* 
+// multiyears["brand"].filter
+const bmw2020 = bmwyears.results.filter(item => {
+
+    return item.tillverkningsar === "2020";
+})
+console.log("antal 2020: " + bmw2020.length)
+
+const bmw2019 = bmwyears.results.filter(item => {
+
+    return item.tillverkningsar === "2019";
+})
+console.log("antal 2019: " + bmw2019.length)
+
+const bmw2018 = bmwyears.results.filter(item => {
+
+    return item.tillverkningsar === "2018";
+})
+console.log("antal 2018: " + bmw2018.length)
+
+ */
+
+const arrAvg = arr => arr.reduce((a, b) => {
+
+    return Number(a) + Number(b)
+}, 0) / arr.length
+
+export const averagePrices = (brand) => {
+    if (!brand) {
+        console.log("Must provide brand");
+        return;
+    }
+    let brand2018Prices = multi[brand].filter(item => (item.tillverkningsar === "2018")).map(item => item.nybilspris);
+    let brand2019Prices = multi[brand].filter(item => (item.tillverkningsar === "2019")).map(item => item.nybilspris);
+    let brand2020Prices = multi[brand].filter(item => (item.tillverkningsar === "2020")).map(item => item.nybilspris);
+    console.log("2018 length: " + brand2018Prices.length)
+    console.log("2019 length: " + brand2019Prices.length)
+    console.log("2020 length: " + brand2020Prices.length)
+
+    return {
+        2018: arrAvg(brand2018Prices), 2019: arrAvg(brand2019Prices),
+        2020: arrAvg(brand2020Prices)
+
+    }
+}
+
+export const getBrands = () => Object.keys(multi) // ["bmw", "dacia", osv]
